bb9ae99b78e69b0f89ea4db70e29c01c09c53f6a
Med Ali Naaoui <chayga00@gmail.com>
2024-04-14 08:29:09 +0100

    choosing movie then quality

--- main.go
+++ main.go
@@ -2,30 +2,110 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
-
 type Movie struct {
+	Index int    `json:"index"`
 	Name string `json:"name"`
 	Url string `json:"url"`
 }
 
+func choseMovie(s string) []Movie{
 
-func main() {
+	query := strings.ReplaceAll(s, " ", "+") 
+	url := fmt.Sprintf("https://ak.sv/search?q=%s", query)
 	c := colly.NewCollector()
+
+	var movies []Movie
+	index := 1
 	c.OnHTML(".entry-box .entry-image a", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		fmt.Println("Visiting:", link)
-		c.Visit(e.Request.AbsoluteURL(link))
-	})
+		movie := Movie{
+			Index: index,
+			Name: e.ChildAttr("img", "alt"),
+			Url: link,
+		}
+		index++
+		movies = append(movies, movie)
+	})	
+	
+	c.Visit(url)
+	// c.OnScraped(func(r *colly.Response) {
+	// 	for i := 0; i < len(movies); i++ {
+	// 		movies[i].Index = i + 1
+	// 	}
+	// })
 
-	c.OnHTML("[data-quality='5']", func(e *colly.HTMLElement) {
-		// Scrape data here
-	})
+	return movies
+}
+
+type Link struct{
+	Index int    `json:"index"`
+	Quality string `json:"quality"`
+	Size string `json:"size"`
+	Url string `json:"url"` 
+}
 
 
-	c.Visit("https://ak.sv/search?q=the+irishman")
-		fmt.Println("hey")
+func getLink(url string) []Link{
+
+	
+	c := colly.NewCollector()
+
+	var links []Link
+	index := 1
+
+	c.OnHTML("[id^='tab-']", func(e *colly.HTMLElement) {
+		url := e.ChildAttr(".link-download", "href")
+
+		quality := ""
+		if e.Attr("id") == "tab-5" {
+			quality = "1080p"
+			} else if e.Attr("id") == "tab-4"{
+				
+				quality = "720p"
+				} else{
+
+					quality = "480p"
+		}
+
+link := Link{
+			Index: index,
+			Quality: quality,
+			Size: e.ChildText(".font-size-14"),
+			Url: url,
+		}
+		index++
+
+		links = append(links, link)
+	})	
+	
+	c.Visit(url)
+	return links
+}
+
+
+func main() {
+	
+	movies := choseMovie("whiplash")
+
+	for _, v := range movies {
+
+		fmt.Printf("\n%v ===> %v\n", v.Index, v.Name)
+	}
+	var choosenMovie int
+	fmt.Print("choose movie: ")
+
+	fmt.Scanln(&choosenMovie)
+
+
+	links := getLink(movies[choosenMovie  - 1].Url)
+
+for _, v := range links {
+		fmt.Printf("\n%v ===> %v ===> %v\n", v.Index, v.Quality, v.Size)
+	}
+
 
 }
